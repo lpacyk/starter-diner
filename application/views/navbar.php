56458ed317ad4bb96b5dbf4cca566c7cbf10936d
Lukasz Pacyk <pacyklukasz@gmail.com>
2016-11-07 21:45:31 -0800

    add link to navbar toggle role Signed-off-by:Lukasz Pacyk <pacyklukasz@gmail.com>

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -16,6 +16,7 @@
 				<li><a href="/">Home</a></li>
 				<li><a href="/hiring">Hiring</a></li>
 				<li><a href="/shopping">Shopping</a></li>
+                                <li><a href="/toggle">Toggle Role</a></li>
             </ul>
             <p class="navbar-text navbar-right">Role: {userrole}</p>
 		</div><!-- /.navbar-collapse -->
