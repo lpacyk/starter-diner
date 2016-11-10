0bc44c585d7cc99c75e931757eedef798f5d1280
inqr <inqr@users.noreply.github.com>
2016-11-10 13:57:40 -0800

    added mtce view and updated crud controller Signed-off-by:inqr <inqr@users.noreply.github.com>

---
+++ application/views/mtce.php
@@ -0,0 +1,14 @@
+<table class='table'>
+        <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Description</th>
+        </tr>
+        {items}
+        <tr>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{description}</td>
+        </tr>
+        {/items}
+</table>
