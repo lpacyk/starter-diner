b6ef819d2c577c34a92cdace5f9515bd42e13896
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-11-10 14:46:43 -0800

    Added edit function to Crud controller. Added mtce-edit view.
    Signed-off-by: Connor Phalen <Phalen.Connor@hotmail.co.uk>

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -1,30 +1,57 @@
 <?php
 class Crud extends Application {
-    
-	function __construct() {
-		parent::__construct();
-	}
-    
-	public function index()	{
-                
-		$role = $this->session->userdata('userrole');
-		$this->data['content'] = '';
-                
-                $this->data['message'] = 'You are a normal user. Please leave this page and go back to the main page.';
-                $this->data['bodymessage'] = 'Yeah.... please leave. I mean, this is an admin-only page so having you here is a bit akward...';
-                    
-                if ($role != 'admin'){
-                    $this->data['message'] = 'You are an admin, please stay!';
-                    $this->data['bodymessage'] = 'WELCOME TO THE mainteannce PAGE. THIS PAGE SHOWS YOU
-                                    ALL OF THE CRAZY AMOUNT OF THINGS THAT HAVE NOT BEEN FIXED
-                                    HERE FOR THE PAST THREE YEARS. THERE IS MORE THAN THAT, BUT WE
-                                    GOT A BUFFER OVERFLOW FOR HAVING TOO MANY PROBLEMS TO FIX, SO 
-                                    WE JUST STOPPED RECORDING THEM. HAVE FUN!';
-                }
-                
-                $this->data['pagebody' ] ='mtce';
-                $this->data['items'] = $this->menu->all();
-                
-                $this->render('template-maintenance'); 
-	}
+
+    function __construct() {
+            parent::__construct();
+            $this->load->helper('formfields');
+    }
+
+    public function index()	
+    {
+        $role = $this->session->userdata('userrole');
+
+        $message = 'You are an admin, please stay!';
+
+        if ($role != 'admin'){
+            $message = 'You are a normal user. Please leave this page and go back to the main page.';
+            $this->data['content'] = $message;
+            $this->render();
+            return;
+        }
+
+        $this->data['pagebody'] ='mtce';
+        $this->data['items'] = $this->menu->all();
+
+        $this->render(); 
+    }
+
+    public function edit($id = null)
+    {
+        $this->session->unset_userdata('key');
+        $this->session->unset_userdata('record');
+        
+        // try the session first
+        $key = $this->session->userdata('key');
+        $record = $this->session->userdata('record');
+        
+        // if not there, get them from the database
+        if (empty($key)) {
+            $record = $this->menu->get($id);
+            $key = $id;
+            $this->session->set_userdata('key',$id);
+            $this->session->set_userdata('record',$record);
+        }
+        
+        // build the form fields
+        $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
+        $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
+        $this->data['fdescription'] = makeTextField('Description', 'description', $record->description);
+        $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
+        $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
+        $this->data['fcategory'] = makeTextField('Category', 'category', $record->category);
+        
+        // show the editing form
+        $this->data['pagebody'] = "mtce-edit";
+        $this->render();
+    }
 }
--- application/views/template.php
+++ application/views/template.php
@@ -11,8 +11,6 @@
 		<div class="container">
 			{navbar}
             {content}
-            <p class="footer">Page rendered in <strong>0.0155</strong> seconds. 
-                {ci_version}</p>
         </div>
        {caboose_scripts}
         {caboose_trailings}
