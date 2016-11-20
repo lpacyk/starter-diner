14c49ab100eae778babcddbc622fa89b8d33b0cf
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-11-20 13:21:08 -0800

    Display and examine the receipts. Shows Order # on receipt.
    Signed-off-by: Connor Phalen <Phalen.Connor@hotmail.co.uk>

--- application/controllers/Shopping.php
+++ application/controllers/Shopping.php
@@ -17,6 +17,23 @@ class Shopping extends Application {
     }
 
     public function summarize() {
+        // identify all of the order files
+        $this->load->helper('directory');
+        $candidates = directory_map('../data/');
+        $parms = array();
+        foreach ($candidates as $filename) {
+           if (substr($filename,0,5) == 'order') {
+               // restore that order object
+               $order = new Order ('../data/' . $filename);
+                // setup view parameters
+               $parms[] = array(
+                   'number' => $order->number,
+                   'datetime' => $order->datetime,
+                   'total' => $order->total()
+                       );
+            }
+        }
+        $this->data['orders'] = $parms;
         $this->data['pagebody'] = 'summary';
         $this->render('template');  // use the default template
     }
@@ -81,6 +98,13 @@ class Shopping extends Application {
         $order->save();
         $this->session->unset_userdata('order');
         redirect('/shopping');
+    }
+    
+    public function examine($which) {
+        $order = new Order ('../data/order' . $which . '.xml');
+        $stuff = $order->receipt();
+        $this->data['content'] = $this->parsedown->parse($stuff);
+        $this->render();
     }
 
 }
